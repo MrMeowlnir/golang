6b66fdeed59a7be8ea1ea574948368b06de8f854
mrmeowlnir <dmsdens@gmail.com>
2022-10-09 19:58:57 +0300

    Lesson 8 Последовательное выполнение тестов с иерархией

--- Lessons/Lesson8/8_sample_test.go
+++ Lessons/Lesson8/8_sample_test.go
@@ -3,11 +3,33 @@ package main
 import "testing"
 
 func TestMult(t *testing.T) {
-	var a, b, result int = 2, 2, 4
+	t.Run("simple", func(t *testing.T) {
+		var a, b, result int = 2, 2, 4
 
-	realResult := Mult(a, b)
+		realResult := Mult(a, b)
 
-	if realResult != result {
-		t.Errorf("Error in Mult(a, b): %d != %d", result, realResult)
-	}
+		if realResult != result {
+			t.Errorf("Error in Mult(a, b): %d != %d", result, realResult)
+		}
+	})
+
+	t.Run("medium", func(t *testing.T) {
+		var a, b, result int = 222, 222, 49284
+
+		realResult := Mult(a, b)
+
+		if realResult != result {
+			t.Errorf("Error in Mult(a, b): %d != %d", result, realResult)
+		}
+	})
+
+	t.Run("negative", func(t *testing.T) {
+		var a, b, result int = -2, 4, -8
+
+		realResult := Mult(a, b)
+
+		if realResult != result {
+			t.Errorf("Error in Mult(a, b): %d != %d", result, realResult)
+		}
+	})
 }

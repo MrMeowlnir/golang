ead21825bb70fed7ec52bcc1cd163b6cb4f1d229
mrmeowlnir <dmsdens@gmail.com>
2022-10-09 22:15:32 +0300

    Lesson 11 panic catching

--- Lessons/Lesson11/11_panic.go
+++ Lessons/Lesson11/11_panic.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 func main() {
-	a := []int{1, 2, 3}
-	c := a[3]
-	panic("AAAAAAAAAAAAAA!")
+	//	a := []int{1, 2, 3}
+	//	c := a[3]
+	//	panic("AAAAAAAAAAAAAA!")
+	divide(1, 0)
+	fmt.Println("Code after panic...")
+}
+
+func divide(a, b int) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Panic!", err)
+		}
+	}()
+	fmt.Println(a / b)
 }

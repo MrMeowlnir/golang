fdeee129b19c2ff5f0bf2f670a1143feb8cf0d97
mrmeowlnir <dmsdens@gmail.com>
2022-10-19 19:05:57 +0300

    create middleware

--- Lessons/Rest-Api/internal/user/handler.go
+++ Lessons/Rest-Api/internal/user/handler.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"Rest-Api/internal/apperror"
 	"net/http"
 
 	"Rest-Api/internal/handlers"
@@ -27,40 +28,46 @@ func NewHandler(logger *logging.Logger) handlers.Handler {
 }
 
 func (h *handler) Register(router *httprouter.Router) {
-	router.HandlerFunc(http.MethodGet, usersURL, h.GetUsersList)
-	router.HandlerFunc(http.MethodGet, userURL, h.GetUserByUUID)
-	router.HandlerFunc(http.MethodPost, userURL, h.CreateUser)
-	router.HandlerFunc(http.MethodPut, userURL, h.UpdateUser)
-	router.HandlerFunc(http.MethodPatch, userURL, h.PartUpdateUser)
-	router.HandlerFunc(http.MethodDelete, userURL, h.DeleteUser)
+	router.HandlerFunc(http.MethodGet, usersURL, apperror.Middleware(h.GetUsersList))
+	router.HandlerFunc(http.MethodGet, userURL, apperror.Middleware(h.GetUserByUUID))
+	router.HandlerFunc(http.MethodPost, userURL, apperror.Middleware(h.CreateUser))
+	router.HandlerFunc(http.MethodPut, userURL, apperror.Middleware(h.UpdateUser))
+	router.HandlerFunc(http.MethodPatch, userURL, apperror.Middleware(h.PartUpdateUser))
+	router.HandlerFunc(http.MethodDelete, userURL, apperror.Middleware(h.DeleteUser))
 }
 
-func (h *handler) GetUsersList(w http.ResponseWriter, r *http.Request) {
+func (h *handler) GetUsersList(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(200)
 	w.Write([]byte("Users List")) // empty block. "WIP"
+	return nil
 }
 
-func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request) {
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(200)
 	w.Write([]byte("Get User By UUID")) // empty block. "WIP"
+	return nil
 }
 
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(201)
 	w.Write([]byte("Create User")) // empty block. "WIP"
+	return nil
 }
 
-func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("Update User")) // empty block. "WIP"
+	return nil
 }
 
-func (h *handler) PartUpdateUser(w http.ResponseWriter, r *http.Request) {
+func (h *handler) PartUpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("Partially Update User")) // empty block. "WIP"
+	return nil
 }
 
-func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("Delete User")) // empty block. "WIP"
+	return nil
 }
